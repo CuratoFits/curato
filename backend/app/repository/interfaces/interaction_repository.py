ef0df3a8cea79d53f868411ae4041cfc6e1220c4
unknown <manushreebartaria07@gmail.com>
2026-09-12 20:01:59 +0530

    repository layer modified, user auth and rest of the cart related functionalities have to be added

--- backend/app/repository/interfaces/interaction_repository.py
+++ backend/app/repository/interfaces/interaction_repository.py
@@ -9,56 +9,29 @@ from app.schemas.interaction import (
 class InteractionRepository(ABC):
 
     @abstractmethod
-    def get_all(
-        self,
-        skip: int = 0,
-        limit: int = 20,
-    ):
+    def get_all(self,skip: int = 0,limit: int = 20)-> list[InteractionEvent]:
         pass
 
     @abstractmethod
-    def get_by_id(
-        self,
-        event_id: int,
-    ):
+    def get_by_id(self,event_id: int) -> InteractionEvent | None:
         pass
 
     @abstractmethod
-    def get_by_user_id(
-        self,
-        user_id: int,
-        skip: int = 0,
-        limit: int = 20,
-    ):
+    def get_by_user_id(self,user_id: int,skip: int = 0,limit: int = 20) -> list[InteractionEvent]:
         pass
 
     @abstractmethod
-    def get_by_product_id(
-        self,
-        product_id: int,
-        skip: int = 0,
-        limit: int = 20,
-    ):
+    def get_by_product_id(self,product_id: int,skip: int = 0,limit: int = 20) -> list[InteractionEvent]:
         pass
 
     @abstractmethod
-    def create(
-        self,
-        interaction: InteractionEventCreate,
-    ):
+    def create(self,interaction: InteractionEventCreate) -> InteractionEvent:
         pass
 
     @abstractmethod
-    def update(
-        self,
-        event_id: int,
-        interaction: InteractionEventUpdate,
-    ):
+    def update(self,event_id: int,interaction: InteractionEventUpdate) -> InteractionEvent:
         pass
 
     @abstractmethod
-    def delete(
-        self,
-        event_id: int,
-    ):
+    def delete(self,event_id: int) -> None:
         pass
